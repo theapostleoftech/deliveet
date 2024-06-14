d41f67d26d146ffde055d4c03213a966f6e36cf7
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-06-14 16:39:51 +0100

    First commit for development

---
+++ pages/urls.py
@@ -0,0 +1,10 @@
+"""
+This holds url routes for all the pages in this application
+"""
+from django.urls import path
+from . import views
+
+app_name = 'pages'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='home'),
+]
---
+++ pages/views.py
@@ -0,0 +1,14 @@
+"""
+This holds views for different pages in the deliveet app
+"""
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+# Create your views here.
+class IndexView(TemplateView):
+    """
+    This is the homepage class-based view
+    """
+    template_name = 'pages/index.html'
+    pass
