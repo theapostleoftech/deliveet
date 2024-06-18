4fb8a1244692e28fd7d4a3c2bcc3fd622c0a4533
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-06-18 06:35:50 +0100

    password change, done

--- accounts/views.py
+++ accounts/views.py
@@ -6,8 +6,10 @@ from django.contrib import messages
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import PasswordChangeView
 from django.contrib.sites import requests
 from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
 from django.views.generic import CreateView, View, DetailView
 
 from accounts.forms import SignUpForm, SignInForm
@@ -78,3 +80,28 @@ class SignInView(View):
                 message = 'Your email or password is incorrect. Please try again.'
                 return render(request, self.template_name, context={'form': form, 'message': message})
 
+
+class ChangePasswordView(LoginRequiredMixin, PasswordChangeView):
+    """
+    This view is used to change the password.
+    """
+    template_name = 'accounts/password/password_change.html'
+    success_url = reverse_lazy('accounts:customer_dashboard')
+
+    def form_valid(self, form):
+        """
+        This function is used to validate the form
+        :param form:
+        :return:
+        """
+        messages.success(self.request, 'Your password has been changed successfully.')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        """
+        This function is used to check if the form is invalid
+        :param form:
+        :return:
+        """
+        messages.error(self.request, "There was an error changing your password. Please try again.")
+        return super().form_invalid(form)
