c28f9856f0b2a63a782f675f70b8143043ef00f4
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-07-04 11:36:32 +0100

    modified decorators

--- deliveet/utils/signals.py
+++ deliveet/utils/signals.py
@@ -1,12 +1,21 @@
+"""
+This module contains modules for
+managing permissions in the deliveet app
+"""
+
 from functools import wraps
 from django.shortcuts import redirect
 from django.contrib import messages
-from django.core.exceptions import PermissionDenied
 
 from accounts.models import UserAccount
 
 
 def customer_required(view_func):
+    """
+    This function checks to see if a user has a customer account
+    before granting access to a page
+    """
+
     @wraps(view_func)
     def _wrapped_view(request, *args, **kwargs):
         if request.user.is_authenticated:
@@ -23,6 +32,11 @@ def customer_required(view_func):
 
 
 def courier_required(view_func):
+    """
+    This function checks to see if a user has a courier account
+    before granting access to a page
+    """
+
     @wraps(view_func)
     def _wrapped_view(request, *args, **kwargs):
         if request.user.is_authenticated:
