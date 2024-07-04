4846963fa23ec048afac5c41633502cdc885d5bf
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-07-04 02:35:01 +0100

    landing page modified

--- pages/urls.py
+++ pages/urls.py
@@ -7,7 +7,6 @@ from . import views
 app_name = 'pages'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='home'),
-    path('sample', views.SampleView.as_view(), name='sample'),
 
-    path('landing/', views.SampleView.as_view(), name='landing_page'),
+    path('landing/', views.LandingPageView.as_view(), name='landing_page'),
 ]
