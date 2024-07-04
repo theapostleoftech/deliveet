a77b70125af2060afc020d0dd7a88b0353a80103
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-07-04 13:44:25 +0100

    courier dashbaord modified

--- customers/views.py
+++ customers/views.py
@@ -3,10 +3,13 @@ This contains views for customer app.
 """
 
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
 
+from deliveet.utils.decorators import customer_required
 
-# Create your views here.
+
+@method_decorator(customer_required, name='dispatch')
 class CustomerDashboardView(TemplateView):
     """
     This view displays the customers dashboard.
